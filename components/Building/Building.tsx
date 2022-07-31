f5fdd5b558979afed9ac48c11b8bcb07fcc81d62
Taylor Pridgen <musicinsilence13@gmail.com>
2022-07-30 18:05:10 -0600

    main/ it wooooorks

--- components/Building/Building.tsx
+++ components/Building/Building.tsx
@@ -3,25 +3,51 @@ import { useState, useEffect } from "react";
 
 type BuildingProps = {
   rotation: number;
+  scale: number;
 };
 
-export const Building = ({ rotation }: BuildingProps) => {
+export const Building = ({ rotation, scale }: BuildingProps) => {
   const [translateValue, setTranslateValue] = useState(rotation);
+  const [scaleValue, setScaleValue] = useState(scale);
 
   useEffect(() => {
     setTranslateValue(rotation);
   }, [rotation]);
 
+  useEffect(() => {
+    setScaleValue(scale);
+  }, [scale]);
+
+  const {
+    building_face,
+    front,
+    top,
+    left,
+    right,
+    roof,
+    front_roof,
+    left_roof,
+    right_roof,
+  } = styles;
+
   return (
     <>
       <div
         className={styles.container}
-        style={{ "--zed": `${translateValue}deg` } as React.CSSProperties}
+        style={
+          {
+            "--zed": `${translateValue}deg`,
+            "--sc": `${scaleValue}px`,
+            "--sct": `${scaleValue / 2}px`,
+          } as React.CSSProperties
+        }
       >
-        <div className={`${styles.building_face} ${styles.front}`}>A</div>
-        <div className={`${styles.building_face} ${styles.top}`}>B</div>
-        <div className={`${styles.building_face} ${styles.left}`}>C</div>
-        <div className={`${styles.building_face} ${styles.right}`}>D</div>
+        <div className={`${building_face} ${front}`}>F</div>
+        <div className={`${building_face} ${left}`}>L</div>
+        <div className={`${building_face} ${right}`}>R</div>
+        <div className={`${building_face} ${roof} ${front_roof}`}>R</div>
+        <div className={`${building_face} ${roof} ${left_roof}`}>RL</div>
+        <div className={`${building_face} ${roof} ${right_roof}`}>RR</div>
       </div>
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,13 @@ import { Building } from "./../components/Building/Building";
 import { useState } from "react";
 import Document from "next/document";
 
+const percentToRange = (percent, rangeMin, rangeMax) => {
+  return percent * ((rangeMax - rangeMin) / 100) + rangeMin;
+};
+
 export default function Home({}) {
   const [mousePos, setMousePos] = useState(30);
+  const [sc, setSc] = useState(100);
 
   return (
     <div
@@ -13,11 +18,16 @@ export default function Home({}) {
         const percentage = e.clientX / window?.innerWidth; // 0.5
         const range = 2 * (percentage * 100) - 100;
         setMousePos(range);
+
+        const p2 = e.clientY / window?.innerHeight;
+        const range2 = percentToRange(p2 * 100, 100, 300);
+        setSc(range2);
       }}
     >
       <div className={styles.greeting}>Hej, I'm Pridgey</div>
       <div className={styles.windowbox}>
-        <Building rotation={mousePos} />
+        <Building rotation={mousePos} scale={sc} />
+        <Building rotation={mousePos / 2} scale={sc * 2} />
       </div>
     </div>
   );
